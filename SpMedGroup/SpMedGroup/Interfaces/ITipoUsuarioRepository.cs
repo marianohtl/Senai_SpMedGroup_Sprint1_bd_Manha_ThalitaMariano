a481e1c7693e21aea233fe9555c00586ed18b6dc
Thalita Mariano <tmariano@eusou.space>
2020-05-09 20:33:50 -0300

    Adicionando Interfaces do Projeto

---
+++ SpMedGroup/SpMedGroup/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using SpMedGroup.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpMedGroup.Interfaces
+{
+    interface ITipoUsuarioRepository
+    {
+     
+    }
+}
